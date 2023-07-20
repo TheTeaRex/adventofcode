76101ae5b6b829acbcefdfca1d751d3d30abf14e
TheTeaRex <thetearex10@gmail.com>
2023-07-20 17:47:41 +0000

    2022 day 8 part 2 advent of code

--- 2022/day_8_treetop_tree_house/code.py
+++ 2022/day_8_treetop_tree_house/code.py
@@ -14,6 +14,7 @@ def map_the_input(text: str) -> List[List[int]]:
     return [[int(c) for c in row] for row in rows]
 
 def solution_part_1(grid: List[List[int]]) -> int:
+    max_scenic_score = 0
     visible_trees = (len(grid) * len(grid[0])) - ((len(grid) - 2) * (len(grid[0]) - 2))
 
     for r in range(1, len(grid) - 1):
@@ -25,41 +26,53 @@ def solution_part_1(grid: List[List[int]]) -> int:
             left = all([tree_height - j > 0 for j in [grid[r][i] for i in range(0, c)]])
             right = all([tree_height - j > 0 for j in [grid[r][i] for i in range(c + 1, len(grid[0]))]])
             """
-            for i in range(0, r):
+            count = [0 for _ in range(4)]
+            for i in range(r - 1, -1, -1):
+                count[0] += 1
                 if tree_height - grid[i][c] <= 0:
                     up = False
                     break
             else:
                 up = True
+                count[0] = r
 
             for i in range(r + 1, len(grid)):
+                count[1] += 1
                 if tree_height - grid[i][c] <= 0:
                     down = False
                     break
             else:
                 down = True
+                count[1] = len(grid) - r - 1
 
-            for i in range(0, c):
+            for i in range(c - 1, -1, -1):
+                count[2] += 1
                 if tree_height - grid[r][i] <= 0:
                     left = False
                     break
             else:
                 left = True
+                count[2] = c
 
             for i in range(c + 1, len(grid[0])):
+                count[3] += 1
                 if tree_height - grid[r][i] <= 0:
                     right = False
                     break
             else:
                 right = True
+                count[3] = len(grid[0]) - c - 1
 
             if any([up, down, left, right]):
                 visible_trees += 1
+            max_scenic_score = max(count[0] * count[1] * count[2] * count[3], max_scenic_score)
 
-    return visible_trees
+    return (visible_trees, max_scenic_score)
 
 if __name__ == "__main__":
     text = read_file()
     # print(text)
     grid = map_the_input(text)
-    print(f'Part 1\'s answer: {solution_part_1(grid)}')
+    answer = solution_part_1(grid)
+    print(f'Part 1\'s answer: {answer[0]}')
+    print(f'Part 2\'s answer: {answer[1]}')
