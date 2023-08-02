26939663e8f5df1a7178c2c5b438727fc1124c89
TheTeaRex <thetearex10@gmail.com>
2023-08-02 06:01:13 +0000

    working on part 2 for 2022 day 9 advent of code

--- 2022/day_9_rope_bridge/code.py
+++ 2022/day_9_rope_bridge/code.py
@@ -36,10 +36,84 @@ def solution_part_1(steps: List[str]) -> int:
                 tailx = oldx
                 taily = oldy
                 visited.add((tailx, taily))
+    return len(visited)
+
+def returns_tail_position(head: List[int], tail: List[int]) -> List[int]:
+    # tail doesn't need to move since it's 1 step within the head in all directions
+    if abs(tail[0] - head[0]) < 1 and abs(tail[1] - head[1]) < 1:
+        return tail
+
+    # tail is on the same row or column as head but 2 steps ahead
+    if tail[0] == head[0]:
+        if tail[1] - head[1] > 1:
+            tail[1] -= 1
+        elif tail[1] - head[1] < -1:
+            tail[1] += 1
+    elif tail[1] == head[1]:
+        if tail[0] - head[0] > 1:
+            tail[0] -= 1
+        elif tail[0] - head[0] < -1:
+            tail[0] += 1
+    # tail is not on the same row or column as head, so move diagonally to get to the head
+    elif (tail[1] - head[1] > 1 and tail[0] - head[0] == 1) or\
+        (tail[0] - head[0] > 1 and tail[1] - head[1] == 1):
+        tail[0] -= 1
+        tail[1] -= 1
+    elif (tail[0] - head[0] > 1 and tail[1] - head[1] == -1) or\
+        (tail[1] - head[1] < -1 and tail[0] - head[0] == 1):
+        tail[0] -= 1
+        tail[1] += 1
+    elif (tail[1] - head[1] < -1 and tail[0] - head[0] == -1) or\
+        (tail[0] - head[0] < -1 and tail[1] - head[1] == -1):
+        tail[0] += 1
+        tail[1] += 1
+    elif (tail[0] - head[0] < -1 and tail[1] - head[1] == 1) or\
+        (tail[1] - head[1] > 1 and tail[0] - head[0] == -1):
+        tail[0] += 1
+        tail[1] -= 1
+
+    return tail
+
+def improved_part_1(steps: List[str], num_knots: int = 2) -> int:
+    coordinates = {
+        'U': {
+            'index': 1,
+            'diff': 1
+        },
+        'R': {
+            'index': 0,
+            'diff': 1
+        },
+        'D': {
+            'index': 1,
+            'diff': -1
+        },
+        'L': {
+            'index': 0,
+            'diff': -1
+        }
+    }
+    snake = []
+    visited = set()
+    for _ in range(num_knots):
+        snake.append([0, 0])
+    visited.add(tuple(snake[-1]))
+    for step in steps:
+        if len(step) == 0:
+            break
+        direction, num = step.split(' ')
+        for _ in range(int(num)):
+            snake[0][coordinates[direction]['index']] += coordinates[direction]['diff']
+            # move the rest of the body per step
+            for i in range(1, len(snake)):
+                snake[i] = returns_tail_position(snake[i - 1], snake[i])
+            # finished moving the snake, add last section to visited
+            visited.add(tuple(snake[-1]))
+            print(snake)
 
     return len(visited)
 
 if __name__ == "__main__":
     text = read_file()
-    steps = text.split('\n')
-    print(solution_part_1(steps))
+    print(solution_part_1(text.split('\n')))
+    print(improved_part_1(text.split('\n'), 10))
