230478d0e399630cb1a8c553a418ec93422017ed
TheTeaRex <thetearex10@gmail.com>
2023-08-03 05:12:48 +0000

    2022 advent of code day 10 part 1

---
+++ 2022/day_10_cathod_ray_tube/code.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python3
+
+import os
+from typing import List
+
+def read_file() -> str:
+    f = open(f'{os.path.dirname(os.path.realpath(__file__))}/input', 'r')
+    text = f.read()
+    f.close()
+    return text
+
+def solution_part_1(instructions: List[str]) -> int:
+    i = result = 0
+    x_val = 1
+    counter = 0
+    cycle_counter = 0
+    reads_instruction = True
+    while i < len(instructions):
+        counter += 1
+
+        if (counter - 20) % 40 == 0:
+            cycle_counter += 1
+            # print(f'x = {x_val}')
+            # print(f'counter = {counter}')
+            result += (x_val * counter)
+            if cycle_counter == 6:
+                break
+
+        if reads_instruction:
+            if instructions[i] != 'noop':
+                num = int(instructions[i].split(' ')[1])
+                reads_instruction = False
+            i += 1
+        else: # don't need to read instruction as it's in a process of a addx
+            # num variable is available
+            x_val += num
+            reads_instruction = True
+
+    return result
+
+
+
+if __name__ == "__main__":
+    text = read_file().split('\n')
+    print(f'Part 1: {solution_part_1(text)}')
