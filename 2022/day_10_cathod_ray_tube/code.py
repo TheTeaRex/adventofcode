d7b31540e2f1a96013b5cf010dca3f40e9060866
TheTeaRex <thetearex10@gmail.com>
2023-08-11 23:25:21 +0000

    finished 2022 day 10 advent of code

--- 2022/day_10_cathod_ray_tube/code.py
+++ 2022/day_10_cathod_ray_tube/code.py
@@ -20,8 +20,6 @@ def solution_part_1(instructions: List[str]) -> int:
 
         if (counter - 20) % 40 == 0:
             cycle_counter += 1
-            # print(f'x = {x_val}')
-            # print(f'counter = {counter}')
             result += (x_val * counter)
             if cycle_counter == 6:
                 break
@@ -38,8 +36,41 @@ def solution_part_1(instructions: List[str]) -> int:
 
     return result
 
+def solution_part_2(instructions: List[str]) -> str:
+    result = ''
+    i = counter = 0
+    cycle_counter = position = 0
+    x_val = 1
+    reads_instruction = True
+    while i < len(instructions):
+        counter += 1
 
+        if (counter - 1) % 40 == 0:
+            cycle_counter += 1
+            result += '\n'
+            position = 0
+            if cycle_counter == 7:
+                break
+
+        if position == x_val or position == x_val - 1 or position == x_val + 1:
+            result += '#'
+        else:
+            result += '.'
+        position += 1
+
+        if reads_instruction:
+            if instructions[i] != 'noop':
+                num = int(instructions[i].split(' ')[1])
+                reads_instruction = False
+            i += 1
+        else: # don't need to read instruction as it's in a process of a addx
+            # num variable is available
+            x_val += num
+            reads_instruction = True
+
+    return result
 
 if __name__ == "__main__":
     text = read_file().split('\n')
     print(f'Part 1: {solution_part_1(text)}')
+    print(f'Part 2: {solution_part_2(text)}')
