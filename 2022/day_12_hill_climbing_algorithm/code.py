2b5a88a8ad1855bd4459fdd068b224edf0c351f5
TheTeaRex <thetearex10@gmail.com>
2023-08-22 22:48:00 +0000

    2022 advent of code day 12 part 2

--- 2022/day_12_hill_climbing_algorithm/code.py
+++ 2022/day_12_hill_climbing_algorithm/code.py
@@ -15,4 +15,5 @@ def read_file(file_name: str) -> str:
 if __name__ == "__main__":
     text = read_file('input').split('\n')
     grid = Grid(text)
-    print(f'Part 1: {grid.fewest_steps} steps')
+    print(f'Part 1: {grid.fewest_steps_from_actual_start} steps')
+    print(f'Part 2: {grid.fewest_steps_from_all_elevation_a} steps')
--- 2022/day_12_hill_climbing_algorithm/grid.py
+++ 2022/day_12_hill_climbing_algorithm/grid.py
@@ -11,8 +11,16 @@ class Grid:
         self.start = None
         self.end = None
         self.find_end_to_end_and_convert_map()
-        self.fewest_steps = self.find_shortest_path()
-
+        self.fewest_steps_from_actual_start = self.find_shortest_path(self.start[0],
+                                                                      self.start[1],
+                                                                      self.end[0],
+                                                                      self.end[1])
+        self.list_of_a = [self.start]
+        self.list_of_a += self.find_all_a_locations()
+        self.list_of_shorts = []
+        for i, j in self.list_of_a:
+            self.list_of_shorts.append(self.find_shortest_path(i, j, self.end[0], self.end[1]))
+        self.fewest_steps_from_all_elevation_a = min(filter(lambda k: k is not None, self.list_of_shorts))
 
 
     def create_grid(self, lines: List[str]) -> List[List[str]]:
@@ -22,7 +30,7 @@ class Grid:
         return grid
 
     def find_end_to_end_and_convert_map(self) -> None:
-        for i, line in enumerate(self.map):
+        for i in range(len(self.map)):
             for j in range(len(self.map[0])):
                 if self.map[i][j] == 'S':
                     self.start = (i, j)
@@ -33,12 +41,20 @@ class Grid:
                 else:
                     self.map[i][j] = ord(self.map[i][j]) % 96 # ord('a') - 1
 
-    def find_shortest_path(self) -> int:
+    def find_all_a_locations(self) -> List[Tuple]:
+        result = []
+        for i in range(len(self.map)):
+            for j in range(len(self.map[0])):
+                if self.map[i][j] == 1:
+                    result.append((i, j))
+        return result
+
+    def find_shortest_path(self, starti: int, startj: int, endi: int, endj: int) -> int:
         visited = [[False for _ in range(len(self.map[0]))] for _ in range(len(self.map))]
-        q = deque([(self.start[0], self.start[1], 0)])
+        q = deque([(starti, startj, 0)])
         while q:
             i, j, step = q.popleft()
-            if (i, j) == self.end:
+            if (i, j) == (endi, endj):
                 return step
             for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                 ni = i + di
