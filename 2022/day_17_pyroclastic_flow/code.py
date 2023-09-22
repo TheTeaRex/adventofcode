a1593918e23a70abaf2ea889b352c1a6363029d3
TheTeaRex <thetearex10@gmail.com>
2023-09-22 20:54:57 +0000

    aoc 2022 day 17 switched to use 7bits rather than 9bits to represent the chamber

--- 2022/day_17_pyroclastic_flow/code.py
+++ 2022/day_17_pyroclastic_flow/code.py
@@ -10,7 +10,6 @@ class Solution(object):
     def __init__(self, filename: str, num: int) -> None:
         self.jet = Jet(self.read_file(filename))
         self.rock_id = 0
-        # self.part_one(num, Chamber())
         self.part_one_solution(num)
 
     def read_file(self, file_name: str) -> str:
@@ -21,29 +20,44 @@ class Solution(object):
 
     def prints_rock(self, rock: List[int]) -> None:
         for i in rock[::-1]:
-            print(format(i, '09b'))
+            print(format(i, '07b'))
 
     def prints_chamber(self, chamber: List[int]) -> None:
         for i in chamber[:-len(chamber):-1]:
-            line = f'{format(i ^ 257, "09b")}'
+            line = f'{format(i, "07b")}'
             line = line.replace('1', '#')
             line = line.replace('0', '.')
             line = '|' + line[1:len(line) - 1] + '|'
             print(line)
         print('+-------+')
 
-    def can_rock_move_sideway_then_down(self, rock: List[int], jet_dir: str, chamber: List[int], level: int):
+    def can_rock_move_sideway_then_down(self, rock: List[int], jet_dir: str, chamber: List[int], level: int) -> List:
+        next_to_wall = False
         if jet_dir == '>':
-            new_position = [i >> 1 for i in rock]
+            # check if next to wall
+            for row in rock:
+                if row & 1 != 0:
+                    next_to_wall = True
+                    break
+            else:
+                new_position = [i >> 1 for i in rock]
         else: # if dir == '<'
-            new_position = [i << 1 for i in rock]
-        # check to see if the sideway new position is valid
-        for i in range(level, level + len(rock)):
-            if new_position[i - level] & chamber[i] != 0:
-                break
-        else:
-            # valid, so get to new position
-            rock = new_position
+            # check if next to wall
+            for row in rock:
+                if row & 64 != 0:
+                    next_to_wall = True
+                    break
+            else:
+                new_position = [i << 1 for i in rock]
+
+        if not next_to_wall:
+            # check to see if the sideway new position is valid
+            for i in range(level, level + len(rock)):
+                if new_position[i - level] & chamber[i] != 0:
+                    break
+            else:
+                # valid, so get to new position
+                rock = new_position
 
         # check to see if can move down 1 unit
         settled = False
@@ -58,25 +72,25 @@ class Solution(object):
                 if rock[i - level] & chamber[i] == 1:
                     raise Exception
                 chamber[i] = rock[i - level] | chamber[i]
-            while chamber[-1] == 257:
+            while chamber[-1] == 0:
                 chamber.pop()
 
-        return (settled, rock, chamber)
+        return [settled, rock, chamber]
 
     def part_one_solution(self, num: int) -> int:
         chamber = [511] # 0b1111111
         rocks = [
-            [60], # 0b0011110
-            [16, 56, 16], # 0b0001000, 0b0011100, 0b0001000
-            [56, 8, 8], # 0b0011100, 0b0000100, 0b0000100
-            [32, 32, 32, 32], # 0b0010000 x4
-            [48, 48] # 0b0011000 x2
+            [30], # 0b0011110
+            [8, 28, 8], # 0b0001000, 0b0011100, 0b0001000
+            [28, 4, 4], # 0b0011100, 0b0000100, 0b0000100
+            [16, 16, 16, 16], # 0b0010000 x4
+            [24, 24] # 0b0011000 x2
         ]
 
         for i in range(num):
             rock = rocks[i % len(rocks)]
-            chamber += [257 for _ in range(len(rock) + 3)]
-            position = len(chamber) -len(rock)
+            chamber += [0 for _ in range(len(rock) + 3)]
+            position = len(chamber) - len(rock)
             while True:
                 jet_dir = self.jet.current
                 settled, rock, chamber = self.can_rock_move_sideway_then_down(rock, jet_dir, chamber, position)
@@ -89,4 +103,4 @@ class Solution(object):
 
 if __name__ == "__main__":
     # Part 1 solution
-    Solution('matt_input', 2022)
+    Solution('input', 2022)
